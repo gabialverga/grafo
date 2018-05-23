f857eeeaf0e9897667dc6773327e6432d213b558
gabialverga <gabrielaalverga@gmail.com>
2018-05-23 02:20:28 -0300

    atualizações

--- monta.js
+++ monta.js
@@ -1,18 +1,17 @@
-function completo(){
+function geraGrafo(){
 	var qnt = document.getElementById("qntNos").value;
-	console.log(qnt);
 	var v = [];
 	var e = [];
 	for(var i=0;i<qnt;i++){
 		v.push({
 			data:{
-				id:i
+				id: i
 			} 
 		});
 	}
-	/*for(var i=0;i<qnt;i++){
+	for(var i=0;i<qnt;i++){
 		for(var j=0;j<qnt;j++){
-			if(j!==i){
+			if(document.getElementById((i*qnt)+j)!==null){
 				e.push({
 					data:{
 						source: i,
@@ -21,88 +20,45 @@ function completo(){
 				});
 			}
 		}
-	}*/
-	if(document.getElementById("00").checked){
-		e.push({
-			data:{
-				source: 0,
-				target: 0
-			}
-		});
 	}
-	if(document.getElementById("01").checked){
-		e.push({
-			data:{
-				source: 0,
-				target: 1
-			}
-		});
-	}
-	if(document.getElementById("02").checked){
-		e.push({
-			data:{
-				source: 0,
-				target: 2
-			}
-		});
-	}
-	if(document.getElementById("10").checked){
-		e.push({
-			data:{
-				source: 1,
-				target: 0
-			}
-		});
-	}
-	if(document.getElementById("11").checked){
-		e.push({
-			data:{
-				source: 1,
-				target: 1
-			}
-		});
-	}
-	if(document.getElementById("12").checked){
-		e.push({
-			data:{
-				source: 1,
-				target: 2
-			}
-		});
-	}
-	if(document.getElementById("20").checked){
-		e.push({
-			data:{
-				source: 2,
-				target: 0
-			}
-		});
-	}
-	if(document.getElementById("21").checked){
-		e.push({
-			data:{
-				source: 2,
-				target: 1
-			}
-		});
-	}
-	if(document.getElementById("22").checked){
-		e.push({
-			data:{
-				source: 2,
-				target: 2
-			}
-		});
-	}
-	var elms = { nodes: v,edges: e};
+	var elms = {nodes: v,edges: e};
 	var cy = cytoscape({
-	    container: document.getElementById('cy'),
-	    elements: elms
+		container: document.getElementById('cy'),
+		elements: elms
 	});
 	cy.layout({
-	    name: 'circle'
+		name: 'circle'
 	}).run();
 }
+
 function matriz(){
-	
+	var qnt = document.getElementById("qntNos").value;
+	var container = document.getElementById("tabela");
+	var html = [];
+	html.push('<table>');
+	html.push('<tr>');
+	html.push('<td></td>');
+	for(var j=0;j<qnt;j++){
+		html.push('<td>');
+		html.push((j+1));
+		html.push('</td>');
+	}
+	html.push('</tr>');
+	for(var i=0;i<qnt;i++){
+		html.push('<tr>');
+		html.push('<td>');
+		html.push(i+1);
+		html.push('</td>');
+		for(var j=0;j<qnt;j++){
+			html.push('<td><input type="checkbox" id = "');
+			html.push((i*qnt)+j);
+			html.push('"></td>');
+		}
+		html.push('</tr>');
+	}
+	html.push('</table>');
+	var gera = document.getElementById("gera");
+	gera.style.visibility = 'visible';
+	container.innerHTML = html.join("\n");
+
 }
