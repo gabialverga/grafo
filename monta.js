c339c423540870c0f1d8c99762fb8e3e3a4c4f6f
gabialverga <gabrielaalverga@gmail.com>
2018-05-23 18:09:23 -0300

    a

--- monta.js
+++ monta.js
@@ -5,14 +5,16 @@ function geraGrafo(){
 	for(var i=0;i<qnt;i++){
 		v.push({
 			data:{
-				id: i
-			} 
+				id: i,
+				name: i
+			}
 		});
 	}
 	for(var i=0;i<qnt;i++){
 		for(var j=0;j<qnt;j++){
 			var id = ((i*qnt)+j).toString();
 			if(document.getElementById(id).checked){
+				document.getElementById(((j*qnt)+i)).checked = true;
 				e.push({
 					data:{
 						source: i,
@@ -36,28 +38,19 @@ function matriz(){
 	var qnt = document.getElementById("qntNos").value;
 	var container = document.getElementById("tabela");
 	var html = [];
-	html.push('<table>');
-	html.push('<tr>');
-	html.push('<td></td>');
+	html.push('<table><tr><td></td>');
 	for(var j=0;j<qnt;j++){
-		html.push('<td>');
-		html.push((j+1));
-		html.push('</td>');
+		html.push('<td>'+(j+1)+'</td>');
 	}
 	html.push('</tr>');
 	for(var i=0;i<qnt;i++){
-		html.push('<tr>');
-		html.push('<td>');
-		html.push(i+1);
-		html.push('</td>');
+		html.push('<tr><td>'+(i+1)+'</td>');
 		for(var j=0;j<qnt;j++){
-			html.push('<td><input type="checkbox" id="' + ((i*qnt)+j) + '"></td>');
+			html.push('<td><input type="checkbox" id="' + ((i*qnt)+j) +'"></td>');
 		}
 		html.push('</tr>');
 	}
 	html.push('</table>');
-	var gera = document.getElementById("gera");
-	gera.style.visibility = 'visible';
+	html.push('<br><input type="button" id ="gera" value="Gerar Grafo" onclick="geraGrafo()"/>');
 	container.innerHTML = html.join("\n");
-
 }
